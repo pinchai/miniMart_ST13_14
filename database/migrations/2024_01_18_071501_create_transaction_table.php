46c94248033a73fa94b2834583918a6fd4bb9b55
Pinchai <>
2024-01-19 10:36:32 +0700

    no message

---
+++ database/migrations/2024_01_18_071501_create_transaction_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->string('invoice_no');
+            $table->integer('type')->comment('1: sale, 2: purchase');
+            $table->dateTime('transaction_date');
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('customer_id');
+            $table->unsignedBigInteger('currency_id');
+            $table->decimal('sell_out_price', 10, 2);
+            $table->unsignedBigInteger('delivery_id');
+            $table->decimal('delivery_fee');
+            $table->decimal('delivery_fee');
+            $table->decimal('grand_total');
+            $table->decimal('total_received');
+            $table->text('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction');
+    }
+};
